a3a9dbdae44e3ca8250ef6aee4016e67bda72210
jy465 <xjiayix04@gmail.com>
2026-04-16 15:47:59 +0800

    Credit level - Todo app with environment variables

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ todos = []
 
 @app.route("/")
 def index():
-    app_name = os.environ.get("APP_NAME", "Focus")
+    app_name = os.environ.get("APP_NAME", "FocusFlow")
     return render_template("index.html", todos=todos, app_name=app_name)
 
 @app.route("/add", methods=["POST"])
